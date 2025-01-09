b315e59a7897b4f37011c92d6dccceb467bd851e
Elena Bardho <ebardho17@epoka.edu.al>
2025-01-09 12:25:41 +0000

    Fix network issues

--- src/app/components/transaction.tsx
+++ src/app/components/transaction.tsx
@@ -3,19 +3,31 @@
 import { useState } from "react";
 import { useWallet } from "@meshsdk/react";
 import { BlockfrostProvider, MeshTxBuilder } from "@meshsdk/core";
+import dotenv from 'dotenv';
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+//dotenv.config({ path: './src/.env' });
+
+
 
 export const TransactionButton = () => {
   const [message, setMessage] = useState("");
 
   const { wallet, connected, name, connect, disconnect, error } = useWallet();
 
-  const blockchainProvider = new BlockfrostProvider('xx');
+  const blockchainProvider = new BlockfrostProvider('');
+  console.log('BLOCKFROST API',process.env.BLOCKFROST_PREVIEW_API);
+  
+  console.log(blockchainProvider);
 
   const txBuilder = new MeshTxBuilder({
     fetcher: blockchainProvider,
     evaluator: blockchainProvider,
     verbose: true,
   });
+
+  txBuilder.setNetwork("preview");
 
   const handleClick = () => {
     setMessage("Transaction Submitted!");
@@ -27,12 +39,15 @@ export const TransactionButton = () => {
     // console.log("UTxOs:", utxos.toString());
     const changeAddress = await wallet.getChangeAddress();
 
+    const network = await wallet.getNetworkId();
+    console.log("Network ID:", network); 
+
     console.log("Building Tx");
     const unsignedTx = await txBuilder
       .changeAddress(changeAddress)
       .selectUtxosFrom(utxos)
       .complete();
-
+//sometimes we don't need to specify the output and it will be input - transaction fee and go to the change address 
     console.log("Signing Tx");
     const signedTx = await wallet.signTx(unsignedTx);
     
