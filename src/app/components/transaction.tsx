442142961927f6d930e0b3ca47a86804d90e608d
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-13 14:10:02 +0000

    remove unneeded stuff

--- src/app/components/transaction.tsx
+++ src/app/components/transaction.tsx
@@ -2,112 +2,68 @@
 
 import { useState } from "react";
 import { useWallet } from "@meshsdk/react";
-import { BlockfrostProvider, MeshTxBuilder } from "@meshsdk/core";
+import { BlockfrostProvider, deserializeAddress, Transaction, TransactionOptions } from "@meshsdk/core";
 import { Button, TextField, Box, Typography, Container, Alert } from "@mui/material";
 
 
 export const TransactionButton = () => {
   const [message, setMessage] = useState("");
-  const [amount, setAmount] = useState("");
-  const [recipientAddress, setRecipientAddress] = useState("");
+  const [unsignedTransaction, setUnsignedTransaction] = useState("");
 
   const { wallet, connected, name, connect, disconnect, error } = useWallet();
 
   // const blockchainProvider = new BlockfrostProvider(process.env.BLOCKFROST_PREVIEW_API||'');
   const blockchainProvider = new BlockfrostProvider('')
 
-
-  const txBuilder = new MeshTxBuilder({
-    fetcher: blockchainProvider,
-    evaluator: blockchainProvider,
-    submitter: blockchainProvider,
-    verbose: true,
-  });
-
-  txBuilder.setNetwork("preview");
-
-  const handleClick = () => {
-    setMessage("Transaction Submitted!");
-  };
-
   const buildAndSubmitTransaction = async () => {
 
-    console.log("Getting UTxOs");
-    const utxos = await wallet.getUtxos();
-    // console.log("UTxOs:", utxos.toString());
+    // Get change address from connected wallet
+    // extract payment and stake credentials from change address
     const changeAddress = await wallet.getChangeAddress();
+    const paymentCred = deserializeAddress(changeAddress).pubKeyHash;
+    console.log("Connected wallet payment credential:", paymentCred);
+
+    const stakeCred = deserializeAddress(changeAddress).stakeCredentialHash;
+    console.log("Connected wallet stake credential:", stakeCred);
 
     const network = await wallet.getNetworkId();
-    console.log("Network ID:", network);
+    console.log("Connected wallet network ID:", network);
 
+    // Try to take the transaction input and pass to wallet to sign
     try {
-      if (!amount || parseFloat(amount) <= 0) {
-        setMessage("Please enter a valid ADA amount.");
-        return;
-      }
 
-      console.log("Building Tx");
-      const unsignedTx = await txBuilder
-        .txOut(recipientAddress, [{ unit: "lovelace", quantity: amount.concat("000000") }]) //convert ada to lovelace
-        //  .txOut(changeAddress, [{ unit: "lovelace", quantity: "1000000" }])
-        .changeAddress(changeAddress)
-        .selectUtxosFrom(utxos)
-        .complete();
-      //sometimes we don't need to specify the output and it will be input - transaction fee and go to the change address 
+      // take the unsigned tx
+      // const unsignedTx = new Transaction({})
+      
+      // display the unsigned tx
+
+      // validate the unsigned tx
+
+      // pass the tx to the wallet to sign
+
       console.log("Signing Tx");
-      const signedTx = await wallet.signTx(unsignedTx);
+      // const signedTx = await wallet.signTx(unsignedTx, true);
 
-      console.log("Tx built successfully:", signedTx);
+      // console.log("Tx built successfully:", signedTx);
 
-      console.log("Submitting Tx");
-      const txHash = await txBuilder.submitTx(signedTx);
-
-      console.log("Transaction submitted successfully:", txHash);
     } catch (error) {
-      console.error("Error building or submitting transaction:", error);
-      setMessage("Transaction failed. Check the console for more details.");
+      console.error("Error signing transaction:", error);
+      setMessage("Transaction signing failed. Check the console for more details.");
     }
   }
 
   return (
-    // <div>
-    //   <input
-    //     type="number"
-    //     placeholder="Enter ADA amount"
-    //     value={amount}
-    //     onChange={(e) => setAmount(e.target.value)}
-    //   />
-    //   <input
-    //     type="string"
-    //     placeholder="Enter address"
-    //     value={recipientAddress}
-    //     onChange={(e) => setRecipientAddress(e.target.value)}
-    //   />
-    //   <button onClick={() => buildAndSubmitTransaction()}>BUILD AND SUBMIT TX</button>
-    //   {message && <p>{message}</p>}
-    // </div>
+
     <Container maxWidth="sm" sx={{ mt: 5 }}>
-      <Typography variant="h4" gutterBottom>
-        ADA Transaction
-      </Typography>
 
       <Box component="form" noValidate autoComplete="off" sx={{ mb: 3 }}>
         <TextField
-          type="number"
-          label="Enter ADA Amount"
+          type="string"
+          label="Enter hex encoded transaction"
           variant="outlined"
           fullWidth
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          sx={{ mb: 2 }}
-        />
-        <TextField
-          type="text"
-          label="Enter Recipient Address"
-          variant="outlined"
-          fullWidth
-          value={recipientAddress}
-          onChange={(e) => setRecipientAddress(e.target.value)}
+          value={unsignedTransaction}
+          onChange={(e) => setUnsignedTransaction(e.target.value)}
           sx={{ mb: 2 }}
         />
         <Button
@@ -116,7 +72,7 @@ export const TransactionButton = () => {
           fullWidth
           onClick={buildAndSubmitTransaction}
         >
-          BUILD AND SUBMIT TX
+          Pass transaction to wallet to sign
         </Button>
       </Box>
 
