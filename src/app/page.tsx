fa60fc88c8c6f94003d2db4993bb384649e76c4f
Elena Bardho <ebardho17@epoka.edu.al>
2025-02-03 11:19:04 +0000

    Fix to pass linter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Wallet } from "./components/wallet";
 import { TransactionButton } from "./components/transaction";
 import { Container, Typography, Box } from "@mui/material";
+import Image from "next/image";
 
 
 export default function Home() {
@@ -30,7 +31,7 @@ export default function Home() {
           justifyContent: "flex-start",
         }}>
           
-          <img style={{width: '15%', height: 'auto', paddingLeft: '10px'}} src="/images/Logo.svg" alt="Logo" />
+          <Image src="/images/Logo.svg" alt="Logo" width={100} height={100} style={{ paddingLeft: 10 }}/>
       
         </Box>
         <Typography variant="h4" component="h1" fontWeight="bold" textTransform="uppercase" mb={3} textAlign="center" paddingTop={2}>
