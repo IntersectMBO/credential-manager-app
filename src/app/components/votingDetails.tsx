9d0952861fe5a75aa2c98114e0fb624a735b1c25
Elena Bardho <ebardho17@epoka.edu.al>
2025-02-25 14:10:15 +0000

    Add checkbox acknowledgement for gov ID

--- src/app/components/votingDetails.tsx
+++ src/app/components/votingDetails.tsx
@@ -1,5 +1,6 @@
-import {Table, TableBody, TableCell, TableContainer, TableRow, Paper, Link } from "@mui/material";
+import {Table, TableBody, TableCell, TableContainer, TableRow, Paper, Link, Checkbox, FormControlLabel } from "@mui/material";
 import { openInNewTab } from "../utils/txUtils";
+import { useState } from "react";
 
 interface VotingDetailsProps {
     govActionID: string;
@@ -7,6 +8,7 @@ interface VotingDetailsProps {
     cardanoscan: string;   
     metadataAnchorURL: string;
     metadataAnchorHash: string;
+    onAcknowledgeChange: (checked: boolean) => void;
 }
 
 export const VotingDetails = ({ 
@@ -14,49 +16,64 @@ export const VotingDetails = ({
     voteChoice, 
     cardanoscan, 
     metadataAnchorURL, 
-    metadataAnchorHash 
+    metadataAnchorHash,
+    onAcknowledgeChange 
 }: VotingDetailsProps) => {
+    const [isChecked,setIsChecked] = useState(false);
+
+    const handleCheckBoxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsChecked(event.target.checked); 
+        onAcknowledgeChange(event.target.checked);
+      };
     return (
-        <TableContainer sx={{ mb: 3 }}>
-<Table sx={{ mt: 3 }}>
-  <TableBody>
-    <TableRow>
-      <TableCell sx={{ fontWeight: "bold" }}>
-        Governance Action ID{" "}
-      </TableCell>
-      <TableCell >
-        <a href={`${cardanoscan}`} target="_blank" style={{ color: 'blue', textDecoration: 'underline' }}>
-          {govActionID}
-        </a>
-      </TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell sx={{ fontWeight: "bold" }}>
-        Vote Choice{" "}
-      </TableCell>
-      <TableCell>{voteChoice}</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell sx={{ fontWeight: "bold" }}>
-        Metadata Anchor URL
-      </TableCell>
-      <TableCell>
-        <Link
-          onClick={() => openInNewTab(metadataAnchorURL||"")}
-          style={{ color: "blue", textDecoration: "underline" }}
-        >
-          {metadataAnchorURL}
-        </Link>
-      </TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell sx={{ fontWeight: "bold" }}>
-        Metadata Anchor Hash
-      </TableCell>
-      <TableCell>{metadataAnchorHash}</TableCell>
-    </TableRow>
-  </TableBody>
-</Table>
-</TableContainer>
+      <TableContainer sx={{ mb: 3 }}>
+        <Table sx={{ mt: 3 }}>
+          <TableBody>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>
+                Governance Action ID{" "}
+              </TableCell>
+              <TableCell>
+                <a
+                  href={`${cardanoscan}`}
+                  target="_blank"
+                  style={{ color: "blue", textDecoration: "underline" }}
+                >
+                  {govActionID}
+                </a>
+              </TableCell>
+              <TableCell>
+                <FormControlLabel
+                  control={<Checkbox checked={isChecked} onChange={handleCheckBoxChange} />}
+                  label="*Acknowledge"
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>Vote Choice </TableCell>
+              <TableCell>{voteChoice}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>
+                Metadata Anchor URL
+              </TableCell>
+              <TableCell>
+                <Link
+                  onClick={() => openInNewTab(metadataAnchorURL || "")}
+                  style={{ color: "blue", textDecoration: "underline" }}
+                >
+                  {metadataAnchorURL}
+                </Link>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>
+                Metadata Anchor Hash
+              </TableCell>
+              <TableCell>{metadataAnchorHash}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     );
 }
